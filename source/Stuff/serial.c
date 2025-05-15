90d442a41b3d3f9c27d8b404527484a5cefae1fb
Kross1de <awesome.hubygu@yandex.ru>
2025-05-15 21:39:30 +0600

    Kernel: FINALLY IDTgit add .!

--- source/Stuff/serial.c
+++ source/Stuff/serial.c
@@ -15,7 +15,7 @@ void serialInstall(void){
 	outb(COM1+4,0x1e);
 	outb(COM1,0x55);
 	
-	assert(inb(COM1)!=0x55);
+	assert(inb(COM1)==0x55);
 		
 	outb(COM1+4,0x0f);
 	printf("%s:%d: enabled port 0x3f8\n",__FILE__,__LINE__);
--- source/kernel.c
+++ source/kernel.c
@@ -1,14 +1,15 @@
 #include "video/VGA.h"
 #include "Stuff/printf.h"
 #include "system/version.h"
-#include "GDT.h"
+#include "Stuff/CPU/GDT.h"
+#include "Stuff/CPU/IDT.h"
 #include "Stuff/serial.h"
 
 void kmain(void *mboot_info) {
 		vga_clear();
 		vga_enable_cursor();
 		printf("%s %d.%d.%d %s %s %s\n",KernelName,KernelVersionMajor,KernelVersionMinor,KernelVersionPatch,KernelBuildDate,KernelBuildTime,KernelArch);
-		dprintf("test");
 		serialInstall();
         gdtInstall();
+        idt_install();
 }
